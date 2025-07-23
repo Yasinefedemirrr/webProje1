bda4e7dc06ea0e43fcd0485e78e70e8ab3768ce1
90545 <yasinefedemir9@gmail.com>
2025-07-23 14:06:09 +0300

    Author api

--- Presentation/API/Program.cs
+++ Presentation/API/Program.cs
@@ -1,5 +1,8 @@
+using Application;
 using Application.interfaces;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistance.Context;
 using Persistance.Repositories.AuthorRepositories;
 using Persistance.Repositories.BookRepositories;
 using Persistance.Repositories.CarGo.Persistence.Repositories;
@@ -7,30 +10,35 @@ using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Controller ve Swagger
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
-builder.Services.AddMediatR(typeof(IAuthorRepository).Assembly);
+// DbContext (SQL Server baðlantýsý)
+builder.Services.AddDbContext<WebProjeContext>(options =>
+    options.UseSqlServer("Server=YASINEFEDEMIR\\SQLEXPRESS;Database=webProje;Trusted_Connection=True;TrustServerCertificate=True;"));
 
+// MediatR (Application katmanýndaki handler'larý taratýyoruz)
+builder.Services.AddMediatR(typeof(AssemblyReference).Assembly);
+
+// Dependency Injection
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 builder.Services.AddScoped<IAuthorRepository, AuthorRepository>();
 builder.Services.AddScoped<IBookRepository, BookRepository>();
 
 var app = builder.Build();
 
-
+// Swagger UI
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+// Middleware pipeline
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
