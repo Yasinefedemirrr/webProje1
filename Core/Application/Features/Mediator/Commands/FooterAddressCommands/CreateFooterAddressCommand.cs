53609be5add94f9ed1d7d3d1477692c2363fc662
90545 <yasinefedemir9@gmail.com>
2025-07-24 11:36:05 +0300

    FooterAdress Mediator

---
+++ Core/Application/Features/Mediator/Commands/FooterAddressCommands/CreateFooterAddressCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Mediator.Commands.FooterAddressCommands
+{
+    public class CreateFooterAddressCommand : IRequest
+    {
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+        public string Email { get; set; }
+    }
+}
