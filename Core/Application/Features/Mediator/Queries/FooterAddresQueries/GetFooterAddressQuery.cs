6d4d87de09b1d45fb9d36c5dbc1f7b06e6677e38
90545 <yasinefedemir9@gmail.com>
2025-07-24 11:32:45 +0300

    FooterAddress Mediator

---
+++ Core/Application/Features/Mediator/Queries/FooterAddresQueries/GetFooterAddressQuery.cs
@@ -0,0 +1,14 @@
+﻿using Application.Features.Mediator.Results.FooterAddressResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Mediator.Queries.FooterAddresQueries
+{
+    public class GetFooterAddressQuery : IRequest<List<GetFooterAddressQueryResult>>
+    {
+    }
+}
