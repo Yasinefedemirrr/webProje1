9810824405578ae41070aef142411378732092ae
90545 <yasinefedemir9@gmail.com>
2025-07-24 10:45:55 +0300

    Book Mediator

---
+++ Core/Application/Features/Mediator/Queries/BookQueries/GetBookByIdQuery.cs
@@ -0,0 +1,20 @@
+﻿using Application.Features.Mediator.Results.BookResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Mediator.Queries.BookQueries
+{
+    public class GetBookByIdQuery : IRequest<GetBookByIdQueryResult>
+    {
+        public int Id { get; set; }
+
+        public GetBookByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
