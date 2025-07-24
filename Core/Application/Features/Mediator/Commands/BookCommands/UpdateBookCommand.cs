c710c483e416630ed7d6c59f5bf1af9237f8a10e
90545 <yasinefedemir9@gmail.com>
2025-07-24 10:48:52 +0300

    Book Mediator

---
+++ Core/Application/Features/Mediator/Commands/BookCommands/UpdateBookCommand.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entity;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Mediator.Commands.BookCommands
+{
+    public class UpdateBookCommand : IRequest
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string FullDescription { get; set; }
+        public string CoverImageUrl { get; set; }
+        public double? Rating { get; set; }
+        public bool IsPopular { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int AuthorId { get; set; }
+        
+    }
+}
