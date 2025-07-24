c9d4a64d6733dcb4bb570d5526c8305b3e96ac98
90545 <yasinefedemir9@gmail.com>
2025-07-24 11:11:12 +0300

    Contact Mediator

---
+++ Core/Application/Features/Mediator/Results/ContactResults/GetContactQueryResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Mediator.Results.ContactResults
+{
+    public class GetContactQueryResult
+    {
+        public int ContactID { get; set; }
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+}
